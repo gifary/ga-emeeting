1ffe5a9aab6eed6a8a08f54ade81c81d9d76b1a5
gifary <gifary.upwork@gmail.com>
2023-02-04 06:12:50 +0700

    add new relation

--- src/Models/User.php
+++ src/Models/User.php
@@ -9,6 +9,8 @@ class User extends Model
 {
     use Notifiable;
 
+    protected $hidden = ['password'];
+
     protected $connection = 'pgsql';
     protected $table = 'user';
 
@@ -19,6 +21,22 @@ class User extends Model
 
     public function internal()
     {
-        return $this->belongsTo(UserInternal::class, 'user_id', 'id');
+        return $this->belongsTo(UserInternal::class, 'id', 'user_id');
+    }
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class, 'user_role', 'user_id', 'role_id');
+    }
+
+    public function scopeRole($query, $role)
+    {
+        return $query->whereHas('roles', function ($q2) use($role) {
+            if (is_array($role)) {
+                $q2->whereIn('description', $role);
+            }else{
+                $q2->whereIn('description', explode(',', $role));
+            }
+        });
     }
 }
